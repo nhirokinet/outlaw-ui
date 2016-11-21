f9b31fa575e2f86771c67bdca1b4b993e0e4bd1e
nhirokinet <nhirokinet@nhiroki.net>
2016-11-21 22:34:18 +0900

    added language descriptions

---
+++ languages.php
@@ -0,0 +1,13 @@
+<?php
+require_once('./lib/lib.php');
+require_once('./parts/header.php');
+
+?>
+<h2>言語ごとの注意事項</h2>
+<h3>Java</h3>
+ソースコードは Main.java というファイル名としてコンパイルされ、 Main クラスが呼び出されます。
+
+<h3>Scala</h3>
+ソースコードは Main.scala というファイル名としてコンパイルされ、 Main クラスが呼び出されます。
+<?php
+require_once('./parts/footer.php');
